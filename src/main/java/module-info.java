12621bd9bf44073f7634bfb5a829e4cbed36c51e
Sebastian-Mateus <juans.mateush@uqvirtual.edu.co>
2025-10-17 07:09:28 -0500

    First
    first

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.poo.parcialii_mendoza_mateus {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.poo.parcialii_mendoza_mateus to javafx.fxml;
+    exports co.edu.uniquindio.poo.parcialii_mendoza_mateus;
+}
