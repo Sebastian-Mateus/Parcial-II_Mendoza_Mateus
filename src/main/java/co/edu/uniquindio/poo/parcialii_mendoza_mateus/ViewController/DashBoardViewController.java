d6dfe52d9e4c75fa22332334f3598de5d5fbe92a
Sebastian-Mateus <juans.mateush@uqvirtual.edu.co>
2025-10-23 22:57:21 -0500

    Prueba
    prueba1

---
+++ src/main/java/co/edu/uniquindio/poo/parcialii_mendoza_mateus/ViewController/DashBoardViewController.java
@@ -0,0 +1,34 @@
+package co.edu.uniquindio.poo.parcialii_mendoza_mateus.ViewController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class DashBoardViewController {
+
+
+
+
+
+        @FXML
+        private Button agregar;
+
+        @FXML
+        private Button listar;
+
+        @FXML
+        private BorderPane mainBorderPane;
+
+        @FXML
+        void agregarInmueble(ActionEvent event) {
+
+        }
+
+        @FXML
+        void listarInmuebles(ActionEvent event) {
+
+        }
+
+
+}
